03b5cf0363519e868267bc72e2331912bf0bb21b
KeshavPokharel <keshavpokharel69@gmail.com>
2023-08-17 19:33:29 +0545

    change some erreo

--- forms/accountCheck.php
+++ forms/accountCheck.php
@@ -8,7 +8,7 @@
         $authToken = $_COOKIE['auth_token'];
 
         // Query to check if the user exists in the riders table based on the auth_token
-        $query = "SELECT rider_id FROM riders WHERE user_id = (SELECT user_id FROM users WHERE token = '$authToken') LIMIT 1";
+        $query = "SELECT rider_id FROM riders WHERE user_id = (SELECT user_id FROM users WHERE token = '$authToken') AND state = 'approved'  LIMIT 1";
 
         $result = $link->query($query);
 
